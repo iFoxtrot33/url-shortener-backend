973eff1b27a489f5adb507434b9415d5efef28a3
Iurii Furman <iuriifurman@MacBook-Pro-Iurii.local>
2025-05-11 20:26:41 +0200

    fix: comments

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -30,12 +30,10 @@ func CORS(allowedOrigins []string) Middleware {
 			}
 
 			if originAllowed {
-				// Устанавливаем CORS заголовки
 				w.Header().Set("Access-Control-Allow-Origin", origin)
 				w.Header().Set("Access-Control-Allow-Credentials", "true")
 			}
 
-			// Обрабатываем предварительные запросы (preflight)
 			if r.Method == http.MethodOptions {
 				w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
@@ -44,7 +42,6 @@ func CORS(allowedOrigins []string) Middleware {
 				return
 			}
 
-			// Пропускаем запрос дальше
 			next.ServeHTTP(w, r)
 		})
 	}
